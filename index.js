a111b344114e06e5706ed4e8973439ab3a288d0b
hp <sahalnurdin@gmail.com>
2022-10-22 12:55:53 +0700

    add some practices

---
+++ index.js
@@ -0,0 +1,16 @@
+// NPM Superheroes
+
+var superheroes = require('superheroes');
+
+var mySuperheroesName = superheroes.random();
+
+console.log(mySuperheroesName);
+
+
+// NPM SuperVillain
+
+var superVillain = require('supervillains');
+
+var mySuperVillainsName = superVillain.random();
+
+console.log(mySuperVillainsName);
